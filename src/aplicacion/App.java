aa100469e93ff0d7ba31a4421741e94e4177a23b
Felipe mac <fconcha.almeida@gmail.com>
2019-09-27 21:43:05 -0300

    fix: version estable

--- src/aplicacion/App.java
+++ src/aplicacion/App.java
@@ -10,14 +10,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
 import modelo.Despacho;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+
  
 /**
  *
@@ -29,16 +26,6 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-
-        ArrayList<Despacho> despachos = new ArrayList<>();
-
-        double latitudExtremoNorte= -91.000000;
-        double latitudExtremoSur = 91.000000;
-        double longitudExtremoEste= -181.000000;
-        double longitudExtremoOeste= 181.00000;
-
-        double oeste, este, norte, sur;
-
         String path = "merlina_legacy.xlsx";
         File file = new File(path);
 
@@ -51,75 +38,6 @@ public class App {
         
         Generador generador = new Generador();
         
-        Iterator<Row> rowIterator = sheet.iterator();
-
-        Row row;
-        Iterator<Cell> cellIterator;
-
-        row = rowIterator.next();
-
-        while (rowIterator.hasNext()) {
-            row = rowIterator.next();
-
-            cellIterator = row.cellIterator();
-            Cell cell;
-
-
-            cell= cellIterator.next();
-            int numeroDespacho= (int)cell.getNumericCellValue();
-
-            cell= cellIterator.next();
-            Date fechaIngreso= cell.getDateCellValue();
-
-            cell= cellIterator.next();
-            String ciudad= cell.getStringCellValue();
-
-            cell= cellIterator.next();
-            double longitud= cell.getNumericCellValue();
-
-            if(longitud< longitudExtremoOeste)
-                oeste= longitud;
-            if(longitud> longitudExtremoEste)
-                este= longitud;
-
-            cell= cellIterator.next();
-            double latitud= cell.getNumericCellValue();
-            if(latitud < latitudExtremoSur)
-                sur= latitud;
-            if(latitud > latitudExtremoNorte)
-                norte= latitud;
-
-            cell= cellIterator.next();
-            String codIso= cell.getStringCellValue();
-
-            cell= cellIterator.next();
-
-            cell= cellIterator.next();
-            int codProducto= (int)cell.getNumericCellValue();
-
-            cell= cellIterator.next();
-            String marca= cell.getStringCellValue();
-
-            cell= cellIterator.next();
-            String nomProducto= cell.getStringCellValue();
-
-            cell= cellIterator.next();
-            double formato= cell.getNumericCellValue();
-
-            cell= cellIterator.next();
-            int unidades= (int)cell.getNumericCellValue();
-
-            cell= cellIterator.next();
-            int valorDespacho= (int)cell.getNumericCellValue();
-
-            cell= cellIterator.next();
-            Date fechaEnvio= cell.getDateCellValue();
-
-            Despacho nuevoDespacho = new Despacho (numeroDespacho, fechaIngreso, ciudad, longitud, latitud, codIso, codProducto, marca, nomProducto, formato, unidades, valorDespacho, fechaEnvio);
-            despachos.add(nuevoDespacho);
-        }
-
-        despachos.forEach((despacho) -> System.out.println(despacho.getNumeroDespacho()));
+        generador.generarListaDespachos(sheet).forEach((Despacho despacho) -> System.out.println(despacho.getNomProducto()));
     }
-    
 }
--- src/controladores/Generador.java
+++ src/controladores/Generador.java
@@ -5,15 +5,97 @@
  */
 package controladores;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import modelo.Despacho;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
 /**
  *
  * @author Pauli
  */
 public class Generador {
+    ArrayList<Despacho> despachos = new ArrayList<>();
+
+    double oeste, este, norte, sur;
+    double latitudExtremoNorte= -91.000000;
+    double latitudExtremoSur = 91.000000;
+    double longitudExtremoEste= -181.000000;
+    double longitudExtremoOeste= 181.00000;
     
     
-    
-    public generarListaDespachos(){
+    public ArrayList<Despacho> generarListaDespachos(XSSFSheet sheet){
+        Iterator<Row> rowIterator = sheet.iterator();
+
+        Row row;
+        Iterator<Cell> cellIterator;
+
+        row = rowIterator.next();
+
+        while (rowIterator.hasNext()) {
+            row = rowIterator.next();
+
+            cellIterator = row.cellIterator();
+            Cell cell;
+
+
+            cell= cellIterator.next();
+            int numeroDespacho= (int)cell.getNumericCellValue();
+
+            cell= cellIterator.next();
+            Date fechaIngreso= cell.getDateCellValue();
+
+            cell= cellIterator.next();
+            String ciudad= cell.getStringCellValue();
+
+            cell= cellIterator.next();
+            double longitud= cell.getNumericCellValue();
+
+            if(longitud< longitudExtremoOeste)
+                oeste= longitud;
+            if(longitud> longitudExtremoEste)
+                este= longitud;
+
+            cell= cellIterator.next();
+            double latitud= cell.getNumericCellValue();
+            if(latitud < latitudExtremoSur)
+                sur= latitud;
+            if(latitud > latitudExtremoNorte)
+                norte= latitud;
+
+            cell= cellIterator.next();
+            String codIso= cell.getStringCellValue();
+
+            cell= cellIterator.next();
+
+            cell= cellIterator.next();
+            int codProducto= (int)cell.getNumericCellValue();
+
+            cell= cellIterator.next();
+            String marca= cell.getStringCellValue();
+
+            cell= cellIterator.next();
+            String nomProducto= cell.getStringCellValue();
+
+            cell= cellIterator.next();
+            double formato= cell.getNumericCellValue();
+
+            cell= cellIterator.next();
+            int unidades= (int)cell.getNumericCellValue();
+
+            cell= cellIterator.next();
+            int valorDespacho= (int)cell.getNumericCellValue();
+
+            cell= cellIterator.next();
+            Date fechaEnvio= cell.getDateCellValue();
+
+            Despacho nuevoDespacho = new Despacho (numeroDespacho, fechaIngreso, ciudad, longitud, latitud, codIso, codProducto, marca, nomProducto, formato, unidades, valorDespacho, fechaEnvio);
+            despachos.add(nuevoDespacho);
+        }
         
+        return despachos;
     }
 }
